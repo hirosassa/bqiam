63073a8e05c1dd629bdd86ead14074574237507a
hirosassa <hiro.sassa@gmail.com>
2021-06-27 07:57:00 +0900

    WIP: add revoke cmd

--- bqrole/project.go
+++ bqrole/project.go
@@ -31,6 +31,7 @@ func PermitProject(role, project string, users []string, yes bool) error {
 		return errors.New("failed to create bigquery Client")
 	}
 
+	fmt.Printf("PERMIT following PROJECT-WIDE permission\n")
 	fmt.Printf("project_id: %s\n", project)
 	fmt.Printf("role:       %s\n", role)
 	fmt.Printf("users:      %s\n", users)
@@ -66,6 +67,46 @@ func PermitProject(role, project string, users []string, yes bool) error {
 	return nil
 }
 
+func RevokeProject(role, project string, users []string) error {
+	ctx := context.Background()
+	client, err := bq.NewClient(ctx, project)
+	if err != nil {
+		return errors.New("failed to create bigquery Client")
+	}
+
+	fmt.Printf("REVOKE following PROJECT-WIDE permission\n")
+	fmt.Printf("project_id: %s\n", project)
+	fmt.Printf("role:       %s\n", role)
+	fmt.Printf("users:      %s\n", users)
+	fmt.Printf("If you proceeds, PROJECT-WIDE permission will be added. Are you sure? [y/n]")
+
+	reader := bufio.NewReader(os.Stdin)
+	res, err := reader.ReadString('\n')
+
+	if err != nil || strings.TrimSpace(res) != "y" {
+		fmt.Println("Abort.")
+		return nil
+	}
+
+	defer client.Close()
+
+	policy, err := FetchCurrentPolicy(project)
+	if err != nil {
+		return fmt.Errorf("failed to fetch current policy: %s", err)
+	}
+
+	// revoke project-wide role if needed
+	for _, user := range users {
+		err = revokeProjectRole(project, user, role, policy)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Revoke %s to %s access as %s\n", user, project, role)
+	}
+
+	return nil
+}
+
 func grantProjectRole(project, user, role string, policy *ProjectPolicy) error {
 	if hasProjectRole(policy, user, role) { // already has roles/viewer
 		log.Info().Msgf("%s already has a role: %s, project: %s. skipped.", user, role, project)
@@ -82,6 +123,27 @@ func grantProjectRole(project, user, role string, policy *ProjectPolicy) error {
 	cmd := fmt.Sprintf("gcloud projects add-iam-policy-binding %s --member %s --role %s", project, member, role)
 	if err := exec.Command("bash", "-c", cmd).Run(); err != nil {
 		return fmt.Errorf("failed to update policy bindings to grant %s %s: %s\n%s", user, role, err, err.(*exec.ExitError).Stderr)
+	}
+
+	return nil
+}
+
+func revokeProjectRole(project, user, role string, policy *ProjectPolicy) error {
+	if !hasProjectRole(policy, user, role) {
+		log.Info().Msgf("%s doesn't have a role: %s, project: %s. skipped.", user, role, project)
+		return nil
+	}
+
+	var member string
+	if isServiceAccount(user) {
+		member = "serviceAccount:" + user
+	} else {
+		member = "user:" + user
+	}
+
+	cmd := fmt.Sprintf("gcloud projects remove-iam-policy-binding %s --member %s --role %s", project, member, role)
+	if err := exec.Command("bash", "-c", cmd).Run(); err != nil {
+		return fmt.Errorf("failed to update policy bindings to revoke %s %s: %s\n%s", user, role, err, err.(*exec.ExitError).Stderr)
 	}
 
 	return nil
